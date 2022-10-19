47eaeaeb02abddf14984169c3d305e578ed805b7
tasheriaj <tasheriaj@gmail.com>
2022-10-18 20:30:36 -0400

    created class

---
+++ src/Gitgit.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Gitgit {
+ public static void main(String[] args) {
+	 Scanner user = new Scanner(System.in);
+	 int num = 0;
+	 
+	 System.out.println("How old are you?");
+	 num = user.nextInt();
+			
+ }
+}
